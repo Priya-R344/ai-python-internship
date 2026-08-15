91d6e6fcafa46ec031ea988f4c358a2eb9fcc72f
Priya-R344 <pj967554@gmail.com>
2026-08-15 21:52:30 +0530

    Update basic_python.py

--- basic_python.py
+++ basic_python.py
@@ -4,10 +4,8 @@ age = 20
 print("Name:", name)
 print("Age:", age)
 
-# List
 languages = ["Python", "Java", "HTML"]
 print("Languages:", languages)
 
-# Loop
 for language in languages:
     print("Learning:", language)
