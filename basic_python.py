0e60855e68d835b99ee24f715a8e05f0d8e66563
Priya-R344 <pj967554@gmail.com>
2026-08-15 21:51:37 +0530

    Create basic_python.py

---
+++ basic_python.py
@@ -0,0 +1,13 @@
+name = "Priya"
+age = 20
+
+print("Name:", name)
+print("Age:", age)
+
+# List
+languages = ["Python", "Java", "HTML"]
+print("Languages:", languages)
+
+# Loop
+for language in languages:
+    print("Learning:", language)
